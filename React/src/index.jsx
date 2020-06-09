2ea3067302f474c917207ff81a24e84c0585ebe0
Kwakcena <rhkrgudwh@gmail.com>
2020-06-09 19:53:49 +0900

    Component의 관심사 분리하기
    Component 내부에서 상태를 관리하는 부분과 상태를 정의하고 활용하는
    부분의 관심사를 나눈다. React의 장점!

--- React/src/index.jsx
+++ React/src/index.jsx
@@ -2,23 +2,12 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 
-function Counter() {
-  // 카운터 컴포넌트 안에서 관리되는 state를 정의, 그 때 사용되는 useState
-  const [state, setState] = useState({
-    count: 0,
-  });
-
-  const { count } = state;
-
-  function handleClick() {
-    setState({
-      count: count + 1,
-    });
-  }
-
-  // 상태가 바뀔 때 그려지는 개념
+function Counter({ count, onClick }) {
+  // 상태를 변화시키고 정의하는 내용이 상태를 보여주는 선언적인 Counter 컴포넌트 안에 다 들어있어 문제다.
+  // 이를 위해 Counter는 count와 onClick 함수를 props로 받는다.
+  // 이로써 화면에 그려주는 내용만 갖고 있는 컴포넌트를 완성할 수 있다.
   return (
-    <button type="button" onClick={handleClick}>
+    <button type="button" onClick={onClick}>
       Click me!
       (
       { count }
@@ -47,14 +36,40 @@ function Buttons() {
   );
 }
 
-function App() {
+// count나 onClick이 어떻게 처리되는지는 Page 컴포넌트는 모른다.
+function Page({ count, onClick }) {
   return (
     <div>
       <p>Hello world!</p>
       <p>Hi!</p>
-      <Counter />
+      <Counter
+        count={count}
+        onClick={onClick}
+      />
       <Buttons />
     </div>
+  );
+}
+
+// App 입장에서는 count 가 변하는 상태에 관심을 갖는다.
+function App() {
+  const [state, setState] = useState({
+    count: 0,
+  });
+
+  const { count } = state;
+
+  function handleClick() {
+    setState({
+      count: count + 1,
+    });
+  }
+
+  return (
+    <Page
+      count={count}
+      onClick={handleClick}
+    />
   );
 }
 
