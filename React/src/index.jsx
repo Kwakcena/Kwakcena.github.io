33f0a97380c8a07f52205616f3b6a44d09d115d5
Kwakcena <rhkrgudwh@gmail.com>
2020-06-09 18:13:39 +0900

    다양한 UI 조각 Component
    화면에 보여 줄 다양한 UI를 조각내서 component라는 개념으로 분리할 수
    있다.

--- React/src/index.jsx
+++ React/src/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Button(value) {
+  return (
+    <button type="button">
+      {value}
+    </button>
+  );
+}
+
+function Buttons() {
+  return (
+    <p>
+      {[1, 2, 3].map((i) => (
+        Button(i)
+      ))}
+    </p>
+  );
+}
+
 const element = (
   <div>
     <p>Hello world!</p>
+    <p>Hi!</p>
+    {renderButtons()}
   </div>
 );
 
