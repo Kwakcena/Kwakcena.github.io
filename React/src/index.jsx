89ee3c5011c4f03dd5a74c7707ad4bd53e9f69ee
Kwakcena <rhkrgudwh@gmail.com>
2020-06-09 18:40:09 +0900

    컴포넌트 내부에서 사용되는 데이터 관리 useState
    우리가 직접 render() 와 같이 어떤 작업을 하면 render()를 호출하는 등의
    복잡한 작업을 선언하지 않고도 "상태가 바뀌면 render를 수행" 해주는
    개념이 react에 존재한다.
    
    이는 useState 라는 react hook 을 이용하면 가능하다.
    
    useState는 상태를 정의하는 react hook으로 상태를 정의하면 해당 데이터의
    값과 그 상태를 변경할 수 있는 함수를 반환한다.
    
    현재 counter 컴포넌트는 내부의 데이터를 다루기 위해 { count : 0 } 이
    들어있는 state 변수와 이 state를 변경할 수 있는 함수인 setState를 갖게
    된다.
    
    상태가 바뀌면 컴포넌트에서 return 값으로 정의한 JSX를 화면에 다시
    그려준다. 단순히 react는 상태를 정의하고 상태를 사용하고 어떻게 그려야
    하는지 정의하면 알아서 상태가 바뀔 때 UI를 갱신해준다.

--- React/src/index.jsx
+++ React/src/index.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-let count = 0;
-
-function handleClick() {
-  console.log('click!');
-  count += 1;
-  console.log(count);
-  render();
-}
 
 function Counter() {
+  // 카운터 컴포넌트 안에서 관리되는 state를 정의, 그 때 사용되는 useState
+  const [state, setState] = useState({
+    count: 0,
+  });
+
+  const { count } = state;
+
+  function handleClick() {
+    setState({
+      count: count + 1,
+    });
+  }
+
+  // 상태가 바뀔 때 그려지는 개념
   return (
     <button type="button" onClick={handleClick}>
       Click me!
@@ -52,11 +58,7 @@ function App() {
   );
 }
 
-function render() {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('app'),
-  );
-}
-
-render();
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
