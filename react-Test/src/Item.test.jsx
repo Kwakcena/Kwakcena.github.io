97f6698f32b6572fabee5ffc1818dbc894f93781
Kwakcena <rhkrgudwh@gmail.com>
2020-06-16 20:41:11 +0900

    component에서 사용되는 event 함수 테스트 하기
    Item 컴포넌트는 onClickDelete 라는 event 함수를 갖고 있다.
    완료 버튼을 누르면 해당 할 일 목록의 id를 인자로 받아 수행되는 함수이다.
    이 함수를 테스트 하는 방법은 아래와 같다.
    
    먼저 testing-library에서 fireEvent 를 import 해 온다.
    이는 어떤 이벤트가 발생할 수 있도록 해 주는 것이다.
    
    handleClick에 jest.fn() 을 할당해주면, 실제 Item 컴포넌트가 props로 받는
    onClickDelete 와 같은 함수를 할당받게 된다.
    
    fireEvent.click(getByText('완료'))
    - getByText('완료') 은 '완료' 라는 text가 포함된 element를 가져온다.
      여기서는 button element를 가져오게 되는 것이다.
    - 이 버튼을 click 하는 이벤트를 발생시키는 것이 fireEvent.click 이다.
    
    expect(handleClick).toBeCalledWith(1)
    - handleClick 이벤트가 발생헀을 때 어떤 인자를 받아서 실행하는지 알고
      싶다면 toBeCalledWith를 사용한다.
    - 실제로 id를 인자로 받으므로 여기서는 1을 인자로 받는것을 테스트 하는
      것이다.

--- react-Test/src/Item.test.jsx
+++ react-Test/src/Item.test.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
+
+// describe - context - it 방법도 있다. 확인해보기.
 
 test('Item', () => {
   const task = {
@@ -10,10 +12,19 @@ test('Item', () => {
     title: '뭐라도 하기',
   };
 
-  const { container } = render((
-    <Item task={task} />
+  // jest가 미리 준비한 함수로 들어간다.
+  const handleClick = jest.fn();
+
+  const { container, getByText } = render((
+    <Item task={task} onClickDelete={handleClick} />
   ));
 
   expect(container).toHaveTextContent('뭐라도 하기');
   expect(container).toHaveTextContent('완료');
+
+  expect(handleClick).not.toBeCalled();
+  fireEvent.click(getByText('완료'));
+
+  //핸들 클릭이 눌려서 불린다. 어떤 인자를 받아서 실행되는지 알기 위해 With를 쓴다.
+  expect(handleClick).toBeCalledWith(1);
 });
