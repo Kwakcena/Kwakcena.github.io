c60b4030209e5e511acb68d260b955becf45ac57
Kwakcena <rhkrgudwh@gmail.com>
2020-06-16 13:24:46 +0900

    Item test 하기
    test 함수 내에서 하나의 할 일 목록 변수값을 만들고, 이 값을 render
    했을 때 여러가지 결과를 container로 받을 수 있는데, 이에 대한 기대값으로
    toHaveTextContent를 하면 어떤 택스트가 들어있기를 기대한다는 방식으로
    테스트를 할 수 있다.
    
    render는 @testing-library/react 에서 가져오고, toHave... 를 쓰기
    위해서는 @testing-library/jest-dom을 import 해야 한다.

---
+++ react-Test/src/Item.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+import Item from './Item'
+
+test('Item', () => {
+  const task = {
+    id: 1,
+    title: '뭐라도 하기',
+  };
+
+  const { container } = render((
+    <Item task={task} />
+  ));
+
+  expect(container).toHaveTextContent('뭐라도 하기');
+});
