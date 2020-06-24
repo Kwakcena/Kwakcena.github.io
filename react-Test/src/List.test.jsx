2af4259598ed00cadf0d87161ccf1f4216782cb2
Kwakcena <rhkrgudwh@gmail.com>
2020-06-24 17:30:01 +0900

    List component test
    describe - it 구조를 이용해 테스트의 흐름을 작성
    
    List 컴포넌트의 경우 task가 있냐 없냐로 테스트를 구분할 수 있다.
    
    - task가 있다면? tasks를 render 하는가? 완료 버튼을 누르면 삭제 함수가
      호출되는가?
    
    - task가 없다면? 화면에 빈 메세지를 표시하는가?

---
+++ react-Test/src/List.test.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import List from './List';
+
+// describe - it => describe('List') => it('renders tasks')
+
+// with tasks
+// - List tenders tasks...
+// - List renders "delete" button to delete a task
+
+// without tasks
+// - List render no tasks message.
+
+describe('List', () => {
+  const handleClickDelete = jest.fn();
+
+  describe('with tasks', () => {
+    const tasks = [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ];
+    it('render tasks', () => {
+      const { getByText } = render((
+        // List 컴포넌트를 사용하는 방법을 서술한다.
+        <List
+          tasks={tasks}
+          onClickDelete={handleClickDelete}
+        />
+      ));
+
+      expect(getByText(/Task-1/)).not.toBeNull();
+      expect(getByText(/Task-2/)).not.toBeNull();
+    });
+
+    it('renders "완료" button to delete a task', () => {
+      const { getAllByText } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClickDelete}
+        />
+      ));
+
+      const buttons = getAllByText('완료');
+      fireEvent.click(buttons[0]);
+      expect(handleClickDelete).toBeCalledWith(1);
+    });
+  });
+
+  describe('without tasks', () => {
+    it('renders no task message', () => {
+      const tasks = [];
+      const { getByText } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClickDelete}
+        />
+      ));
+      expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+    });
+  });
+});
