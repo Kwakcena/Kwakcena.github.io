da9e4b2f79d396c4f177fb43cf8270b91b61ec35
Kwakcena <rhkrgudwh@gmail.com>
2020-06-25 00:59:34 +0900

    개발환경) React, React-dom 설치
    React를 이용해 DOM을 조작하기 위해 react를 설치함

---
+++ Redux/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = (
+  <div>
+    <p>Hello React</p>
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('app'),
+);
