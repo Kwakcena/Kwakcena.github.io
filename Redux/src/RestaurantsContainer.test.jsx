c3b82419eedcc63fb9d948fa9786a4b16f8f9ce2
Kwakcena <rhkrgudwh@gmail.com>
2020-06-30 20:34:57 +0900

    [GREEN] 가짜 useSelector를 만들어 사용
    __mocks__ 폴더에 가짜 함수를 만들어 놓지 않으면 테스트가 실패한다.
    jest.mock('react-redux')는 없어도 테스트가 통과하긴 하는데, 이는
    'react-redux' 파일에 가짜가 있다는걸 표현하는걸까 ?

--- Redux/src/RestaurantsContainer.test.jsx
+++ Redux/src/RestaurantsContainer.test.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
+jest.mock('react-redux');
+
 test('RestaurantsContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      {
+        id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
+      },
+    ],
+  }));
+
   const { getByText } = render((
     <RestaurantsContainer />
   ));
