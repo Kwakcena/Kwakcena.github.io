98227006ad6028637d19c0154983242b4b08492d
Kwakcena <rhkrgudwh@gmail.com>
2020-06-30 18:42:54 +0900

    [Refactor] 레스토랑 목록을 보여줄 컴포넌트 생성
    레스토랑의 목록을 보여줄 컴포넌트를 따로 분리함.

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Restaurants from './Restaurants';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>김밥제국</li>
-      </ul>
+      <Restaurants />
     </div>
   );
 }
