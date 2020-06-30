9ae5243c30c4c6d2c619900a59a0eef2c3273145
Kwakcena <rhkrgudwh@gmail.com>
2020-06-30 21:03:25 +0900

    [GREEN] 가짜 useSelector를 만들어 사용
    실제 App 컴포넌트는 Provider로 감싸져 있어서 store를 전달하기 때문에
    useSelector를 사용하여 상태를 가져다 사용하는 것이 가능하다.
    
    하지만 App.test에는 Provider로 감싸져 있지 않기 때문에 불가능하다.
    따라서 useSelector를 가짜로 만들어 사용하여 테스트를 통과시킨다.
    
    어떤 부분을 무조건 패스하거나 특정 원하는 값을 무조건 리턴하게 만들어
    우리가 진짜 테스트하고자 하는 부분에 집중할 수 있게 한다.

--- Redux/src/App.test.jsx
+++ Redux/src/App.test.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+import restaurants from '../fixtures/restaurants';
+
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
+
   const { getByText } = render((
     <App />
   ));
