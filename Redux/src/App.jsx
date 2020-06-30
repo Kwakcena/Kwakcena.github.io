4833957e60550618e4cd67e97a6a0104be46cf52
Kwakcena <rhkrgudwh@gmail.com>
2020-06-30 21:17:51 +0900

    [RED] useEffect를 사용해 초기 상태 가져오기
    추후에 배울 리덕스 비동기에서 초기데이터 세팅에 많이 쓰는 패턴이다.

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import {
+  setRestaurants,
+} from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+  // TODO: 초기 실행
+  // restaurants에 데이터 넣기 => dispatch
+
+  useEffect(() => {
+    // 컴포넌트가 만들어 질 때 내부의 함수를 실행
+    dispatch(setRestaurants(restaurants));
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
