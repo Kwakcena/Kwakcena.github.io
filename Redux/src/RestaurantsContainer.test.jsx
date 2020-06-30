dc7ef1e9d6a24a628b9e84750a8693430f3a3e46
Kwakcena <rhkrgudwh@gmail.com>
2020-06-30 20:38:49 +0900

    [Refactor] 테스트용 데이터 fixtures
    fixtures라는 폴더를 만들어 테스트 데이터를 모아 놓는다.

--- Redux/src/RestaurantsContainer.test.jsx
+++ Redux/src/RestaurantsContainer.test.jsx
@@ -6,15 +6,13 @@ import { useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import restaurants from '../fixtures/restaurants';
+
 jest.mock('react-redux');
 
 test('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      {
-        id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
-      },
-    ],
+    restaurants,
   }));
 
   const { getByText } = render((
