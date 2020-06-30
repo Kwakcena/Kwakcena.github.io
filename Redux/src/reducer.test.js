5e2fcadf224b6748e58c8add01e28bca41db49c7
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 02:34:51 +0900

    [RED] dispatch로 레스토랑정보 가져오기 테스트
    레스토랑 목록을 가져오면 레스토랑 정보를 담는 배열의 길이가 0이 아니게
    되는 테스트를 진행

--- Redux/src/reducer.test.js
+++ Redux/src/reducer.test.js
@@ -3,9 +3,16 @@ import reducer from './reducer';
 import {
   setRestaurants,
 } from './actions';
+import restaurants from '../fixtures/restaurants';
 
 describe('reducer', () => {
   describe('setRestaurants', () => {
-
+    it('change restaurants array', () => {
+      const initialState = {
+        restaurants: [],
+      };
+      const state = reducer(initialState, setRestaurants(restaurants));
+      expect(state.restaurants).not.toHaveLength(0);
+    });
   });
 });
