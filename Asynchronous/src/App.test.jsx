324b354c79d44e978db80c702ca369485a77ffe7
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 18:40:56 +0900

    5장 수업 준비

---
+++ Asynchronous/src/App.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import App from './App';
+
+jest.mock('react-redux');
+
+test('App', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [],
+    restaurant: {},
+  }));
+
+  const { queryByText } = render((
+    <App />
+  ));
+
+  expect(dispatch).toBeCalledWith({
+    type: 'setRestaurants',
+    payload: { restaurants: [] },
+  });
+
+  expect(queryByText(/김밥제국/)).toBeNull();
+});
