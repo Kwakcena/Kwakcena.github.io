761e714ea054832a7bff3001b183b8a5d8983336
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 19:06:48 +0900

    [RED] Categories 컴포넌트 생성
    카테고리 컴포넌트를 생성해서 화면에 카테고리 목록을 보여주는 컴포넌트를
    작성한다. 현재는 Categories라는 컴포넌트가 존재하지 않으므로 테스트가
    깨진다.

--- Asynchronous/src/CategoriesContainer.jsx
+++ Asynchronous/src/CategoriesContainer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+import Categories from './Categories';
+
 export default function CategoriesContainer() {
+  const { categories } = useSelector((state) => ({
+    categories: state.categories,
+  }));
+
   return (
-    <p>한식</p>
+    <Categories categories={categories} />
   );
 }
