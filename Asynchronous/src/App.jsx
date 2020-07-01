14376b65bb7ae594e2d771b6918382a85489335f
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 19:51:12 +0900

    [RED] thunk 이용해서 비동기 액션 만들기
    기존의 reducer는 새로운 객체를 반환해주도록 되어 있다.
    하지만 thunk를 쓰면 비동기 액션을 만들어서 상태를 업데이트 하도록 할 수
    있다.
    
    기존에는
    1. loadCategories 함수에 dispatch 를 전달
    2. loadCategories에서 fetchCategories를 통해 서버에서 데이터 가져옴
    3. 가져온 데이터를 action creater인 setCategories에 전달함
    4. 이를 dispatch 하여 reducer를 통해 새로운 상태를 받음.
    
    이제 thunk를 쓰면 dispatch(loadCategories()) 라고 써서 이를 해결하도록
    한다. 인자인 loadCategories는 비동기 액션으로 actions 에서 반환하던
    객체가 아닌 함수 형태를 반환하여 dispatch를 사용하도록 한다.

--- Asynchronous/src/App.jsx
+++ Asynchronous/src/App.jsx
@@ -30,7 +30,12 @@ export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    loadCategories({ dispatch });
+    // 원래는 액션을 넣어주면 기존상태에서 새로운 상태를 만들어 주는데,
+    // loadCategories라는 액션을 통해 dispatch가 비동기 액션에
+    // 대한 처리를 하고 상태를 업데이트 하도록 한다.
+    // 이를 위해 redux thunk를 사용한다.
+    dispatch(loadCategories());
+    // loadCategories({ dispatch });
     loadRestaurants({ dispatch });
   }, []);
 
