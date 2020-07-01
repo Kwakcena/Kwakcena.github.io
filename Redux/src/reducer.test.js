9adb4d4ef2060d1ac854fec7bf6b2bc50b7c799a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 12:45:03 +0900

    [RED] reducer test
    changeRestaurantField 실제 동작을 테스트 한다.

--- Redux/src/reducer.test.js
+++ Redux/src/reducer.test.js
@@ -2,6 +2,8 @@ import reducer from './reducer';
 
 import {
   setRestaurants,
+  changeRestaurantField,
+  addRestaurant,
 } from './actions';
 import restaurants from '../fixtures/restaurants';
 
@@ -13,6 +15,23 @@ describe('reducer', () => {
       };
       const state = reducer(initialState, setRestaurants(restaurants));
       expect(state.restaurants).not.toHaveLength(0);
+    });
+  });
+
+  describe('changeRestaurantField', () => {
+    it('change restaurant form', () => {
+      const initialState = {
+        restaurant: {
+          name: '이름',
+          category: '분류',
+          address: '주소',
+        },
+      };
+      const state = reducer(initialState, changeRestaurantField({
+        name: 'name',
+        value: '마법사주방',
+      }));
+      expect(state.restaurant.name).toBe('마법사주방');
     });
   });
 });
