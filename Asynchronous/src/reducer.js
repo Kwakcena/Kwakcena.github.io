be938c192c7770474b2c34a667ce9b744d0fab54
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 19:22:52 +0900

    [GREEN] reducer test
    카테고리가 추가된 새로운 상태를 반환할 수 있도록 리듀서에 추가하여
    테스트를 통과했다.
    
    지금까지는 API에서 받아 온 카테고리가 아닌 내부적으로 작성한 fixtures에
    의한 값이지만 이제 서버에서 받아오는 기능을 추가해보자.

--- Asynchronous/src/reducer.js
+++ Asynchronous/src/reducer.js
@@ -39,5 +39,14 @@ export default function reducer(state = initalState, action) {
       restaurant: initialRestaurant,
     };
   }
+
+  if (action.type === 'setCategories') {
+    const { categories } = action.payload;
+    return {
+      ...state,
+      categories,
+    };
+  }
+
   return state;
 }
