93184d9b0b9535f2fd8faba7a5b1f181cce70e0a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 19:31:38 +0900

    [GREEN] services 폴더의 api
    async는 그 함수의 반환값을 Promise로 만들어 준다. fetchCategories를
    부르는 loadCategories 에서도 fetchCategories가 끝나길 기다린 다음에
    dispatch를 해야 하므로 fetchCategories도 async를 적용해야 한다.

---
+++ Asynchronous/src/services/api.js
@@ -0,0 +1,10 @@
+export async function fetchCategories() {
+  // TODO: fetch GET /categories
+  // REST - CRUD => Read - collection / member, element
+  return [];
+}
+
+// TODO: delete this!
+export function xxx() {
+
+}
