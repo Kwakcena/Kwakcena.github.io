2797388a9f957c141307be342b17a67a4bf2ea65
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 19:38:38 +0900

    [GREEN] mock 추가
    App test에서도 fetch에 대한 부분을 통과할 수 있도록 가짜를 만든다.

--- Asynchronous/src/App.test.jsx
+++ Asynchronous/src/App.test.jsx
@@ -7,6 +7,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import App from './App';
 
 jest.mock('react-redux');
+jest.mock('./services/api');
 
 test('App', () => {
   const dispatch = jest.fn();
