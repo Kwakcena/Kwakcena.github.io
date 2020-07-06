ee055c8184dd7097e4c4d8a19aa8e9bdf7ec903e
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 15:44:03 +0900

    App test 리팩토링
    App test가 통과할 수 있도록 리팩토링

--- Asynchronous/src/App.test.jsx
+++ Asynchronous/src/App.test.jsx
@@ -2,27 +2,18 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
-jest.mock('./services/api');
-
 test('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector({
-    restaurants: [],
-    restaurant: {},
-    categories: [],
-  }));
 
   const { queryByText } = render((
     <App />
   ));
 
-  expect(dispatch).toBeCalledTimes(2);
-
-  expect(queryByText(/김밥제국/)).toBeNull();
+  expect(dispatch).toBeCalled();
 });
