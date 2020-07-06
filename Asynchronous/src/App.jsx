c0d4e82bc4b2e617e0975fbcfdce7a722441573d
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 15:13:06 +0900

    풀이 강의 조건과 일치하도록 파일 삭제

--- Asynchronous/src/App.jsx
+++ Asynchronous/src/App.jsx
@@ -1,34 +1,9 @@
-import React, { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-import RestaurantCreateContainer from './RestaurantCreateContainer';
-
-import {
-  loadRestaurants,
-  loadCategories,
-} from './actions';
+import React from 'react';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // 원래는 액션을 넣어주면 기존상태에서 새로운 상태를 만들어 주는데,
-    // loadCategories라는 액션을 통해 dispatch가 비동기 액션에
-    // 대한 처리를 하고 상태를 업데이트 하도록 한다.
-    // 이를 위해 redux thunk를 사용한다.
-    dispatch(loadCategories());
-    dispatch(loadRestaurants());
-  }, []);
-
   return (
     <div>
-      <h1>Restaurants</h1>
-      <CategoriesContainer />
-      <RestaurantsContainer />
-      <RestaurantCreateContainer />
+      <p>Hello, world!</p>
     </div>
   );
 }
