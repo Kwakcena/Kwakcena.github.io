e2d8f5cfea0df41929741bc51fa533cf92475d5a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 01:32:58 +0900

    index.jsx로 browserRouter를 뺄 수 있다.

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {
-  BrowserRouter,
   Switch,
   Route,
 } from 'react-router-dom';
@@ -12,22 +11,12 @@ import NotFoundPage from './NotFoundPage';
 import AboutPage from './AboutPage';
 
 export default function App() {
-  // const { location: { pathname } } = window;
-
-  // const MyComponent = {
-  //   '/': HomePage,
-  //   '/about': AboutPage,
-  //   '/restaurants': RestaurantsPage,
-  // }[pathname] || NotFoundPage;
-
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/restaurants" component={RestaurantsPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/restaurants" component={RestaurantsPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 }
