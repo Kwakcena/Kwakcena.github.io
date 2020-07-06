8d6e35196b85ddf50c1057f83687926891ad7e3c
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 00:35:26 +0900

    라우팅 간단하게 해보기
    window.location.pathname 을 이용하면 현재 url 주소의 경로 이름을 얻을 수
    있다. 경로 이름을 얻어 객체의 key 값으로 전달하여 그에 맞는 컴포넌트를
    할당해주어 간단하게 라우팅을 해볼 수 있다.

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -2,8 +2,40 @@ import React from 'react';
 
 import RestaurantsPage from './RestaurantsPage';
 
-export default function App() {
+function HomePage() {
   return (
-    <RestaurantsPage />
+    <div>
+      <h1>Home</h1>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/restaurants">Restaurants</a></li>
+      </ul>
+    </div>
+  );
+}
+
+function NotFoundPage() {
+  return (
+    <p>404 Not Found</p>
+  );
+}
+
+function AboutPage() {
+  return (
+    <p>About...</p>
+  );
+}
+
+export default function App() {
+  const { location: { pathname } } = window;
+
+  const MyComponent = {
+    '/': HomePage,
+    '/about': AboutPage,
+    '/restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
+
+  return (
+    <MyComponent />
   );
 }
--- Router/src/App.test.jsx
+++ Router/src/App.test.jsx
@@ -11,23 +11,12 @@ test('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector(({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-    selectedRegion: null,
+    regions: [],
+    categories: [],
+    restaurants: [],
   })));
 
-  const { queryByText } = render((
+  render((
     <App />
   ));
-
-  expect(dispatch).toBeCalled();
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
 });
