b8412b525481f882d0c74361065863fde4283806
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 16:06:58 +0900

    RegionsContainer 실제 사용 및 리팩토링
    - action.type이 없는 경우 defaultReducer를 사용하도록 리팩토링
    - regions 데이터를 요청하는 api 작성 및 그에 대한 mock 함수도 작성

--- Asynchronous/src/App.jsx
+++ Asynchronous/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import RegionsContainer from './RegionsContainer';
+
 import {
   loadInitialData,
 } from './actions';
@@ -10,10 +12,6 @@ import {
 // 1. 지역 선택 - Regions <- API (0)
 // 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
 // 3. 식당 목록 - Restaurants <- API (with region, category) (1과 2 모두 완료된 경우)
-
-function RegionsContainer() {
-  return null;
-}
 
 function CategoriesContainer() {
   return null;
