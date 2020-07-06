9a1cdf6a2cffaffe02b0883144bdb4b9b6b1b32a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 00:56:26 +0900

    페이지를 컴포넌트로 분리

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -2,29 +2,9 @@ import React from 'react';
 
 import RestaurantsPage from './RestaurantsPage';
 
-function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
-      </ul>
-    </div>
-  );
-}
-
-function NotFoundPage() {
-  return (
-    <p>404 Not Found</p>
-  );
-}
-
-function AboutPage() {
-  return (
-    <p>About...</p>
-  );
-}
+import HomePage from './HomePage';
+import NotFoundPage from './NotFoundPage';
+import AboutPage from './AboutPage';
 
 export default function App() {
   const { location: { pathname } } = window;
