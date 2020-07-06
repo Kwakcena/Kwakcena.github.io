0a3548cf7081e4f88754737c01df02c58af0ddd9
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 15:16:02 +0900

    우리가 해야할 것
    해야 할 목록을 적어 봤다.
    
    각각의 함수들은 따로 컴포넌트를 작성할 것들이다.

--- Asynchronous/src/App.jsx
+++ Asynchronous/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+// 0. 지역, 분류 목록을 얻기
+// 1. 지역 선택 - Regions <- API (0)
+// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
+// 3. 식당 목록 - Restaurants <- API (with region, category) (1과 2 모두 완료된 경우)
+
+function RegionsContainer() {
+  return null;
+}
+
+function CategoriesContainer() {
+  return null;
+}
+function RestaurantsContainer() {
+  return null;
+}
+
 export default function App() {
   return (
     <div>
-      <p>Hello, world!</p>
+      <RegionsContainer />
+      <CategoriesContainer />
+      <RestaurantsContainer />
     </div>
   );
 }
