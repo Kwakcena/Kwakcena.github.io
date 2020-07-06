a3939004b63dbdf2703cab4d8569b838b12fe993
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 16:51:13 +0900

    CategoriesContainer 테스트 구현
    카테고리 목록이 보이도록 구현

---
+++ Asynchronous/src/CategoriesContainer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+// selectCategory,
+} from './actions';
+
+export default function CategoriesContainer() {
+  const dispatch = useDispatch();
+
+  const { categories, selectedCategory } = useSelector((state) => ({
+    categories: state.categories,
+    selectedCategory: state.selectedCategory,
+  }));
+
+  function handleClick(categoryId) {
+    // dispatch(selectCategory(categoryId));
+  }
+
+  return (
+    <ul>
+      {categories.map((category) => (
+        <li key={category.id}>
+          <button
+            type="button"
+            onClick={() => handleClick(category.id)}
+          >
+            {category.name}
+            {/* {selectedCategory ? (
+              <>
+                {category.id === selectedCategory.id ? '(V)' : null}
+              </>
+            ) : null} */}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
