9fe80ba68f5d5709c1949bebdbe7483062674582
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 16:11:06 +0900

    RegionsContainer refactoring
    버튼으로 클릭할 수 있도록 refactoring

--- Asynchronous/src/RegionsContainer.jsx
+++ Asynchronous/src/RegionsContainer.jsx
@@ -3,12 +3,23 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 export default function RegionsContainer() {
+  function handleClick() {
+    return null;
+  }
+
   const regions = useSelector((state) => state.regions);
 
   return (
     <ul>
       {regions.map((region) => (
-        <li key={region.id}>{region.name}</li>
+        <li key={region.id}>
+          <button
+            type="button"
+            onClick={handleClick}
+          >
+            {region.name}
+          </button>
+        </li>
       ))}
     </ul>
   );
