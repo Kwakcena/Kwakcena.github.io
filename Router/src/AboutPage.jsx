32cc295cc16706c351e8963258533e15bd1f8dd9
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 01:33:21 +0900

    Router를 테스트 하는 방법
    MemoryRouter를 이용하여 컴포넌트를 묶어주면 라우팅을 테스트 할 수 있다.
    
    <MemoryRouter initialEntries={[path]}><컴포넌트><MemoryRouter/>
    원하는 경로를 path에 배열 형태의 값으로 넣어 준다.

--- Router/src/AboutPage.jsx
+++ Router/src/AboutPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function AboutPage() {
   return (
-    <p>About...</p>
+    <div>
+      <h1>이 프로그램에 대해서...</h1>
+      <p>이 프로그램은 영국에서 시작되어 20명에게 추천해야 합니다...</p>
+    </div>
   );
 }
--- Router/src/App.test.jsx
+++ Router/src/App.test.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
-test('App', () => {
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector(({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  })));
+describe('App', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector(({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    })));
+  });
 
-  render((
-    <App />
-  ));
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { container } = renderApp({ path: '/' });
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { container } = renderApp({ path: '/about' });
+      expect(container).toHaveTextContent('20명에게 추천');
+    });
+  });
+
+  // context('with path /restaurants', () => {
+  //   it('renders the restaurants page', () => {
+  //     const { container } = renderApp({ path: '/restaurants' });
+  //     expect(container).toHaveTextContent('서울');
+  //   });
+  // });
 });
