04317d3359884860f7d3cecdcbec9f32b087e192
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 15:58:19 +0900

    RegionsContainer 테스트
    regions 목록 중 하나가 보여지기를 테스트 함.

---
+++ Asynchronous/src/RegionsContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+export default function RegionsContainer() {
+  return (
+    <div>
+      regions
+    </div>
+  );
+}
