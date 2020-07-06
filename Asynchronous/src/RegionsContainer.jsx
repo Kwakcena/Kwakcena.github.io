bed6713f1866fdb2168982d65fa94655a20d5656
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 17:19:48 +0900

    Regions에 utils 사용

--- Asynchronous/src/RegionsContainer.jsx
+++ Asynchronous/src/RegionsContainer.jsx
@@ -6,13 +6,13 @@ import {
   selectRegion,
 } from './actions';
 
+import { get } from './utils';
+
 export default function RegionsContainer() {
   const dispatch = useDispatch();
 
-  const { regions, selectedRegion } = useSelector((state) => ({
-    regions: state.regions,
-    selectedRegion: state.selectedRegion,
-  }));
+  const regions = useSelector(get('regions'));
+  const selectedRegion = useSelector(get('selectedRegion'));
 
   function handleClick(regionId) {
     dispatch(selectRegion(regionId));
