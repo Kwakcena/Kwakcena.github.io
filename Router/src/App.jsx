90fab2a4eaf47dd450854857231f4559db8b4992
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 01:10:50 +0900

    React router dom 사용 방법
    - <BrowserRouter>
    브라우져 라우터 컴포넌트로 내부에 Switch, Route 컴포넌트를 묶어준다.
    
    - <Switch>
    내부의 Route 컴포넌트를 선택하여 해당 페이지로 라우팅 해 준다.
    
    - <Route>
    어느 경로인지 지정해준다.
    속성 중 exact는 path가 정확히 일치하는 경우를 말한다.
    
    또한 SPA를 구현하기 위해 Link 라는 컴포넌트를 사용하기도 한다.
    a 태그를 이용하면 페이지를 이동할 때 마다 네트워크로 해당 js 파일을
    요청한다. 하지만 Link를 사용하면 최초에 모든 js 파일을 가져온 다음
    페이지 이동시 네트워크 요청을 하지 않아 더 빠르게 작동할 수 있다.

--- Router/src/App.jsx
+++ Router/src/App.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 
-import RestaurantsPage from './RestaurantsPage';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
+import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 import NotFoundPage from './NotFoundPage';
 import AboutPage from './AboutPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
+  // const { location: { pathname } } = window;
 
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
+  // const MyComponent = {
+  //   '/': HomePage,
+  //   '/about': AboutPage,
+  //   '/restaurants': RestaurantsPage,
+  // }[pathname] || NotFoundPage;
 
   return (
-    <MyComponent />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
