7243495ea73e5a7e784787acf7d388224febd845
Kwakcena <rhkrgudwh@gmail.com>
2020-07-06 15:48:15 +0900

    reducer 테스트 setCategories
    카테고리 목록을 set 하는 테스트 추가

--- Asynchronous/src/reducer.test.js
+++ Asynchronous/src/reducer.test.js
@@ -20,4 +20,20 @@ describe('reducer', () => {
       expect(state.regions).toHaveLength(1);
     });
   });
+
+  describe('setCategories', () => {
+    it('changes categories', () => {
+      const initialState = {
+        categories: [],
+      };
+
+      const categories = [
+        { id: 1, name: '한식' },
+      ];
+
+      const state = reducer(initialState, setCategories(categories));
+
+      expect(state.categories).toHaveLength(1);
+    });
+  });
 });
