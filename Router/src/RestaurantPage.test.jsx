e51074249d4f616fea2407f97e47d2cd9ff97bde
Kwakcena <rhkrgudwh@gmail.com>
2020-07-13 01:25:31 +0900

    RestaurantPage 테스트 통과 시키기
    RestaurantPage 내부에서 사용되는 컴포넌트에 useSelector와 useDispatch를
    사용하고 있으므로 이에 대한 mock을 만들어 주어 테스트를 통과시킨다.

--- Router/src/RestaurantPage.test.jsx
+++ Router/src/RestaurantPage.test.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
 import RestaurantPage from './RestaurantPage';
 
+jest.mock('react-redux');
 describe('RestaurantPage', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: {
+        id: 1,
+        name: '마법사 주방',
+      },
+    }));
+  });
+
   it('renders name', () => {
     const params = { id: '1' };
 
