dc54398968907ef6d5b45ff44b5e1378a1fc55ab
Kwakcena <rhkrgudwh@gmail.com>
2020-07-13 01:01:45 +0900

    useParams 의 사용
    useParams를 사용하면 url로 들어온 값들의 params를 얻을 수 있다.
    
    현재는 url에 설정한 파라미터 중 id가 존재하므로 id 값을 얻어 화면에
    보여지게 할 수 있다.
    
    prop으로 params가 있으면 전달받은 params를 사용하고 없다면
    RestaurantPage 내부에서 useParam을 사용하여 params를 얻는다.

--- Router/src/RestaurantPage.jsx
+++ Router/src/RestaurantPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function RestaurantPage() {
+export default function RestaurantPage({ params }) {
+  const { id } = params || useParams();
   return (
     <div>
-      레스토랑
+      {`레스토랑 ${id}`}
     </div>
   );
 }
