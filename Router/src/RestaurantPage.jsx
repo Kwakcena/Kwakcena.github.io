ae2b806149fc161e727178fdae90da28b9ff77b9
Kwakcena <rhkrgudwh@gmail.com>
2020-07-13 01:19:14 +0900

    관심사를 분리하기
    RestaurantPage
    라우터 컴포넌트로부터 받은 url의 파라미터 값에 대해
    useParam를 사용하여 얻을 수 있다. 이는 리덕스와 UI에 관해서는 몰라도
    된다.
    
    RestaurantContainer
    리덕스를 알고 있으며 Page 컴포넌트로부터 전달받은 id값을 통해 레스토랑
    정보를 요청할 수 있다.
    
    RestaurantDetail
    컨테이너로부터 받은 레스토랑 정보를 화면에 보여준다. 리덕스를 몰라도
    된다.

--- Router/src/RestaurantPage.jsx
+++ Router/src/RestaurantPage.jsx
@@ -2,21 +2,41 @@ import React, { useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { loadRestaurant } from './actions';
 
-export default function RestaurantPage({ params }) {
-  const dispatch = useDispatch();
-  const { id } = params || useParams();
+import { get } from './utils';
 
-  useEffect(() => {
-    dispatch(loadRestaurant({ restaurantId: id }));
-  }, []);
-
+// 리덕스를 몰라도 되며, 화면에 보여주는 것만 신경쓰면 된다.
+function RestaurantDetail({ restaurant }) {
   return (
     <div>
-      {`레스토랑 ${id}`}
+      {`레스토랑 ${restaurant.id}`}
     </div>
+  );
+}
+
+// 리덕스를 알고 있으며 화면에 보여지는 부분은 몰라도 된다.
+function RestaurantContainer({ restaurantId }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurant({ restaurantId }));
+  }, []);
+
+  const restaurant = useSelector(get('restaurant'));
+
+  return (
+    <RestaurantDetail restaurant={restaurant} />
+  );
+}
+
+// 라우터에 대해서만 알고 있으면 된다.
+export default function RestaurantPage({ params }) {
+  const { id } = params || useParams();
+
+  return (
+    <RestaurantContainer restaurantId={id} />
   );
 }
