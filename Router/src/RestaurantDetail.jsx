0d4f2834d39ae3884e9841aeabb6c6accfdc3387
Kwakcena <rhkrgudwh@gmail.com>
2020-07-13 02:06:41 +0900

    MenuItems 분리하기

--- Router/src/RestaurantDetail.jsx
+++ Router/src/RestaurantDetail.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import MenuItems from './MenuItems';
+
 // 리덕스를 몰라도 되며, 화면에 보여주는 것만 신경쓰면 된다.
 export default function RestaurantDetail({ restaurant }) {
+  const { name, address, menuItems } = restaurant;
+
   return (
     <div>
-      <p>{restaurant.name}</p>
-      <p>{restaurant.address}</p>
+      <h2>{name}</h2>
+      <p>{`주소: ${address}`}</p>
+      <h3>메뉴</h3>
+      <MenuItems menuItems={menuItems} />
     </div>
   );
 }
