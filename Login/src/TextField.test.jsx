6b8a8dfa1221c185f2c230961140aaadbed19b6c
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 02:48:59 +0900

    ReviewForm에 중복되는 label, input 처리
    ReviewForm을 보면 중복되는 것 처럼 label과 input이 들어 있다.
    
    이를 하나의 컴포넌트로 분리하여 중복을 처리할 수 있다.

---
+++ Login/src/TextField.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import TextField from './TextField';
+
+describe('TextField', () => {
+  it('renders label and input control', () => {
+    const handleChange = jest.fn();
+
+    const { queryByLabelText } = render((
+      <TextField
+        label="평점"
+        type="number"
+        name="score"
+        onChange={handleChange}
+      />
+    ));
+
+    expect(queryByLabelText('평점')).not.toBeNull();
+  });
+});
