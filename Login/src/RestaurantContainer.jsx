7f0b5d5b8ba10a6fed5b75544204d3a65e1dcfbc
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 02:15:48 +0900

    handleChange 구현
    RestaurantContainer에서 만들어진 handleChange는 내부에 dispatch가 들어
    있다. 이를 ReviewForm의 prop으로 전달하여 텍스트가 바뀔 때마다 onChange
    이벤트로 수행되도록 한다.
    
    따라서 text가 바뀌면 onChange, 즉 Container에서 전달한 handleChange가
    수행되며 changeReviewField 액션이 dispatch로 실행될 것이다.

--- Login/src/RestaurantContainer.jsx
+++ Login/src/RestaurantContainer.jsx
@@ -6,11 +6,17 @@ import RestaurantDetail from './RestaurantDetail';
 
 import {
   loadRestaurant,
+  changeReviewField,
 } from './actions';
 
 import { get } from './utils';
 
-function ReviewForm() {
+function ReviewForm({ onChange }) {
+  function handleChange(event) {
+    const { target: { name, value } } = event;
+    onChange({ name, value });
+  }
+
   return (
     <>
       <div>
@@ -20,6 +26,8 @@ function ReviewForm() {
         <input
           type="number"
           id="review-score"
+          name="score"
+          onChange={handleChange}
         />
       </div>
       <div>
@@ -29,6 +37,8 @@ function ReviewForm() {
         <input
           type="text"
           id="review-description"
+          name="description"
+          onChange={handleChange}
         />
       </div>
     </>
@@ -50,10 +60,16 @@ export default function RestaurantContainer({ restaurantId }) {
     );
   }
 
+  function handleChange({ name, value }) {
+    dispatch(changeReviewField({ name, value }));
+  }
+
   return (
     <>
       <RestaurantDetail restaurant={restaurant} />
-      <ReviewForm />
+      <ReviewForm
+        onChange={handleChange}
+      />
     </>
   );
 }
