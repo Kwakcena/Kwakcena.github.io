9457556842cd0c3603c21c54210308f91d621f6a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 02:32:26 +0900

    Refactor] 평점과 리뷰내용 중복되는 테스트 처리
    controls 라는 변수로 묶어 중복되는 코드를 처리한다.

--- Login/src/RestaurantContainer.test.jsx
+++ Login/src/RestaurantContainer.test.jsx
@@ -47,25 +47,23 @@ describe('RestaurantContainer', () => {
       expect(container).toHaveTextContent('서울시');
     });
 
-    it('renders review write form', () => {
+    it('listens change events', () => {
       const { getByLabelText } = renderRestaurantContainer();
 
-      fireEvent.change(getByLabelText('평점'), {
-        target: { value: '5' },
-      });
+      const controls = [
+        { label: '평점', name: 'score', value: '5' },
+        { label: '리뷰 내용', name: 'description', value: '정말 최고 :)' },
+      ];
 
-      expect(dispatch).toBeCalledWith({
-        type: 'changeReviewField',
-        payload: { name: 'score', value: '5' },
-      });
+      controls.forEach(({ label, name, value }) => {
+        fireEvent.change(getByLabelText(label), {
+          target: { value },
+        });
 
-      fireEvent.change(getByLabelText('리뷰 내용'), {
-        target: { value: '정말 최고 :)' },
-      });
-
-      expect(dispatch).toBeCalledWith({
-        type: 'changeReviewField',
-        payload: { name: 'description', value: '정말 최고 :)' },
+        expect(dispatch).toBeCalledWith({
+          type: 'changeReviewField',
+          payload: { name, value },
+        });
       });
     });
   });
