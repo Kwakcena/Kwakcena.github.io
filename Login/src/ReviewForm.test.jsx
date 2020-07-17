97142a47c8ad15030b7ff9374a33bdee984e85bd
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 02:42:05 +0900

    ReviewForm handleChange 이벤트 호출 테스트
    ReviewForm에서는 dispatch가 아닌 handleChange 이벤트가 호출된다.

--- Login/src/ReviewForm.test.jsx
+++ Login/src/ReviewForm.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ReviewForm from './ReviewForm';
 
@@ -15,5 +15,27 @@ describe('ReviewForm', () => {
 
     expect(queryByLabelText('평점')).not.toBeNull();
     expect(queryByLabelText('리뷰 내용')).not.toBeNull();
+  });
+
+  it('listens change events', () => {
+    const handleChange = jest.fn();
+    const { getByLabelText } = render((
+      <ReviewForm
+        onChange={handleChange}
+      />
+    ));
+
+    const controls = [
+      { label: '평점', name: 'score', value: '5' },
+      { label: '리뷰 내용', name: 'description', value: '정말 최고 :)' },
+    ];
+
+    controls.forEach(({ label, name, value }) => {
+      fireEvent.change(getByLabelText(label), {
+        target: { value },
+      });
+
+      expect(handleChange).toBeCalledWith({ name, value });
+    });
   });
 });
