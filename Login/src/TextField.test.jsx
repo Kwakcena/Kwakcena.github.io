17ee5b872cafd7ffaf229bd63f36e3c93f7d3cd3
Kwakcena <rhkrgudwh@gmail.com>
2020-07-18 02:59:59 +0900

    Refactor] 타입이 있을때와 없을때로 나누기

--- Login/src/TextField.test.jsx
+++ Login/src/TextField.test.jsx
@@ -5,18 +5,52 @@ import { render } from '@testing-library/react';
 import TextField from './TextField';
 
 describe('TextField', () => {
-  it('renders label and input control', () => {
-    const handleChange = jest.fn();
+  context('with type', () => {
+    function renderTextField() {
+      const handleChange = jest.fn();
+      return render((
+        <TextField
+          label="평점"
+          type="number"
+          name="score"
+          onChange={handleChange}
+        />
+      ));
+    }
+    it('renders label and input control', () => {
+      const { queryByLabelText } = renderTextField();
 
-    const { queryByLabelText } = render((
-      <TextField
-        label="평점"
-        type="number"
-        name="score"
-        onChange={handleChange}
-      />
-    ));
+      expect(queryByLabelText('평점')).not.toBeNull();
+    });
 
-    expect(queryByLabelText('평점')).not.toBeNull();
+    it('renders "number" input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="number"');
+    });
+  });
+
+  context('without type', () => {
+    function renderTextField() {
+      const handleChange = jest.fn();
+      return render((
+        <TextField
+          label="리뷰 설명"
+          name="description"
+          onChange={handleChange}
+        />
+      ));
+    }
+    it('renders label and input control', () => {
+      const { queryByLabelText } = renderTextField();
+
+      expect(queryByLabelText('리뷰 설명')).not.toBeNull();
+    });
+
+    it('renders "text" input control', () => {
+      const { container } = renderTextField();
+
+      expect(container).toContainHTML('type="text"');
+    });
   });
 });
