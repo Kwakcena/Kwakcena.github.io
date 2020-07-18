feaccdbac82ca5e4945be938d1ba544cc2ec723c
Kwakcena <rhkrgudwh@gmail.com>
2020-07-19 00:10:14 +0900

    LogoutForm 버튼 클릭시 호출 이벤트 테스트

--- Login/src/LogoutForm.test.jsx
+++ Login/src/LogoutForm.test.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LogoutForm from './LogoutForm';
 
 jest.mock('react-redux');
 
 describe('LogoutForm', () => {
-  it('renders "Log out" button', () => {
+  it('renders "Log out" button and listens click event', () => {
     const handleClick = jest.fn();
 
-    const { container } = render(<LogoutForm onClick={handleClick} />);
+    const { container, getByText } = render((
+      <LogoutForm onClick={handleClick} />
+    ));
 
     expect(container).toHaveTextContent('Log out');
+
+    fireEvent.click(getByText('Log out'));
+
+    expect(handleClick).toBeCalled();
   });
 });
